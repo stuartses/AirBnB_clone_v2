dd1f9a8062a1b1d6877edaa96f9cdf5bc4882a03
oscargomez87 <oscarstiven87@gmail.com>
2020-03-31 16:19:43 -0500

    Modified do_create method for console.py to receive parameters base_model.py correctly receives these parameters and creates new objects

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,10 @@ class BaseModel:
             updated_at: updated date
         """
         if kwargs:
+            if "created_at" or "updated_at" not in kwargs.keys():
+                self.created_at = self.updated_at = datetime.now()
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
@@ -29,7 +33,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         """returns a string
