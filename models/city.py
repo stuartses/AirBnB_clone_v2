941f60e24f8e91d60541724e8626f0568c48052f
stuartses <stuart.ses@hotmail.com>
2020-04-01 11:03:13 -0500

    Task 6

--- models/city.py
+++ models/city.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel
+from models.base_model import Base
+import sqlalchemy
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = sqlalchemy.Column(sqlalchemy.String(length=128), nullable=False)
+    state_id = sqlalchemy.Column(sqlalchemy.String(length=60),
+                                 sqlalchemy.ForeignKey('states.id'),
+                                 nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
 """This is the state class"""
+import models
 from models.base_model import BaseModel
+from models.base_model import Base
+from models.city import City
+import sqlalchemy
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
+        cities: list of cities in state
     """
-    name = ""
+    __tablename__ = "states"
+    name = sqlalchemy.Column(sqlalchemy.String(length=128), nullable=False)
+    cities = sqlalchemy.relationship('City', backref='state',
+                                     cascade='all, delete')
+
+    @property
+    def cities(self):
+        """getter attribute cities that returns the list of City instances
+           with state_id equals to the current State.id
+        Return:
+            list of cities
+        """
+        list_cities = []
+        all_cities = models.storage.all(City)
+        for city_item in all_cities.items():
+            if city_item.state_id == self.id:
+                list_cities.append(city_item)
+
+        return list_cities
