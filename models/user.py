eb674118dd55ed063aa97bf7000db7ac3d63ba3d
Stuart Echeverry <55112420+stuartses@users.noreply.github.com>
2020-04-02 10:00:19 -0500

    Quit "length" word in string type column

--- models/user.py
+++ models/user.py
@@ -18,10 +18,10 @@ class User(BaseModel, Base):
     """
     __tablename__ = "users"
 
-    email = Column(String(length=128), nullable=False)
-    password = Column(String(length=128), nullable=False)
-    first_name = Column(String(length=128), nullable=False)
-    last_name = Column(String(length=128), nullable=False)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
     places = relationship('Place', backref='user',
                           cascade='all, delete-orphan')
     reviews = relationship('Review', backref='user',
