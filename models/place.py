f99b1f959a71d1e5ae35840c8eea168e2603f3b3
stuartses <stuart.ses@hotmail.com>
2020-04-02 15:21:01 -0500

    Task 9, 10

--- models/place.py
+++ models/place.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """This is the place class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Float, Integer
+from models.review import Review
+from sqlalchemy import Table, Column, String, ForeignKey, Float, Integer
 from sqlalchemy.orm import relationship
 from os import environ
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'), primary_key=True),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), primary_key=True))
 
 
 class Place(BaseModel, Base):
@@ -20,8 +28,9 @@ class Place(BaseModel, Base):
         price_by_night:: pice for a staying in int
         latitude: latitude in flaot
         longitude: longitude in float
-        reviews: list of rewiews
         amenity_ids: list of Amenity ids
+        reviews: list of rewiews
+        amenities: relationship many to many with Amenity
     """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
@@ -34,16 +43,20 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    amenity_ids = []
+
     if environ.get('HBNB_TYPE_STORAGE') == "db":
         cities = relationship('City', foreign_keys=[city_id],
                               back_populates='places')
         user = relationship('User', foreign_keys=[user_id],
                             back_populates='places')
+        amenities = relationship("Amenity ", secondary=place_amenity,
+                                 viewonly=False)
     else:
         @property
         def reviews(self):
-            """getter attribute reviews that returns the list of Review instances
-            with place_id equals to the current Place.id
+            """getter attribute reviews that returns the list of Review
+            instances with place_id equals to the current Place.id
             Return:
             list of reviews
             """
@@ -54,3 +67,27 @@ class Place(BaseModel, Base):
                     list_review.append(review_item)
 
             return list_review
+
+        @property
+        def amenities(self):
+            """Getter attribute amenities that returns the list of Amenity
+            instances based on the attribute amenity_ids that contains all
+            Amenity.id linked to the Place
+
+            Return:
+            list of amenities
+            """
+            list_amenities = []
+            for amenity_obj in amenity_ids:
+                if amenity_obj.id == self.id:
+                    list_amenities.append(amenity_obj)
+
+            return list_amenities
+
+        @amenities.setter
+        def amenities(self, amenity_obj):
+            """Setter attribute amenities that handles append method for adding
+            an Amenity.id to the attribute amenity_ids
+            """
+            if amenity_obj.__class__.name == "Amenity":
+                self.amenity_ids.append(amenity_obj)
