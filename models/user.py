146545be20581d37c4248e194ff7b3aee52177bc
stuartses <stuart.ses@hotmail.com>
2020-04-01 21:05:40 -0500

    task 7

---
+++ models/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This is the user class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        __tablename__: name of mySQL table
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+        places: relationship with Place
+        reviews: relationship with Review
+    """
+    __tablename__ = "users"
+    email = Column(String(length=128), nullable=False)
+    password = Column(String(length=128), nullable=False)
+    first_name = Column(String(length=128), nullable=False)
+    last_name = Column(String(length=128), nullable=False)
+    places = relationship('Place', backref='user', cascade='all, delete')
+    reviews = relationship('Review', backref='user', cascade='all, delete')
