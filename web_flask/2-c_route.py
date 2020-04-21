b20c7e351ca95d602c912978abba0057507a7861
stuartses <stuart.ses@hotmail.com>
2020-04-21 17:58:15 -0500

    put a varianble as url

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+1. HBNB
+Start Execution of Flask web application on 0.0.0.0, port 5000
+With the routes '/' and '/hnbn'
+"""
+
+from flask import Flask
+app = Flask('web_flask')
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Creates index page"""
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Creates hbnb page"""
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c_text(text):
+    """show the text after /c/"""
+
+    return 'C %s' % text.replace("_", " ")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
